349ab49ca09fb28b0cf221a833980fd109001455
selman adanir <adanirselman@gmail.com>
2022-07-11 17:04:32 +0300

    Create xcode project named SimpsonBook

---
+++ SimpsonBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpsonBook
+//
+//  Created by Selman ADANİR on 11.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
